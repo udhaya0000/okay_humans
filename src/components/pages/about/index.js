ca01fba516f4c17df104114b41a58ac5d0f6b604
udhayakumar <udhaya0000@gmail.com>
2022-07-05 16:39:21 +0530

    storefront added

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -9,20 +9,7 @@ const About = () => {
     <DefaultLayout className="about">
       <div className="bg-lighter pb-4">
         <Container fluid>
-          <Row className="about__row-1">
-            <Col md="5" className="mb-4 mb-sm-0">
-              
-            </Col>
-            <Col md="7">
-              <div className="d-flex align-items-center ps-sm-5 justify-content-sm-center">
-                <img
-                  src="https://uploads-ssl.webflow.com/59de7f3f07bb6700016482bc/621a6ad2371ac979e17501e8_4895778.svg"
-                  className="img-fluid"
-                  alt=""
-                />
-              </div>
-            </Col>
-          </Row>
+          
         </Container>
       </div>
 
--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,6 +2,7 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 
 import DefaultLayout from "@templates/default-layout";
 import Banner from "@organisms/banner";
+import StorefrontImg from "@assets/images/storefront.png";
 
 import "./style.scss";
 
@@ -9,6 +10,34 @@ const Home = () => {
   return (
     <DefaultLayout className="about">
       <Banner />
+
+      <section className="storefront-section">
+        <Container fluid className="mw-960">
+          <div className="storefront-title text-center">
+            Want to won an Okay Humans
+          </div>
+          <img
+            src={StorefrontImg}
+            alt=""
+            className="storefront-img img-fluid"
+          />
+          <h2 className="section-title or__divider">
+            <span>Here’s what you need to know</span>
+          </h2>
+          <p className="mb-3 lh-1.5 text-center fw-light">
+            Hi there! We’re so pleased you’re interested in an Okay Humans
+            franchise. Below you’ll find a whole bunch of helpful information
+            about us, and what it takes to become an Operating Partner. There’s
+            an application at the bottom of the page, but if you wanna cut to
+            the chase, click here!
+          </p>
+          <div className="d-flex align-items-center justify-content-center">
+            <Button variant="secondary" className="text-uppercase">
+              Take me to application
+            </Button>
+          </div>
+        </Container>
+      </section>
     </DefaultLayout>
   );
 };
